f251a97985c75798f255489b664f3cc3ce723501
Jazmin Arias Salazar <jariass@ucenfotec.ac.cr>
2020-11-28 09:05:36 -0600

    Initial commit

---
+++ Lab9ParteIII/src/test/java/com/cenfotec/seguridad/basica/demo/SeguridadSpringBasicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.seguridad.basica.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadSpringBasicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
